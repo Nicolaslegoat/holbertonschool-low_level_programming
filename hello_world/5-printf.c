1b7b55b5dfd8de86155ba6cc1dcfcec44311637a
root <Nicolaslegoat>
2023-06-16 04:52:24 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+*  main - Description pour main
+*  Return: Always 0 (succes)
+*/
+
+int main() {
+	printf("with proper grammar, but the outcome is a piece of art,")
+}
