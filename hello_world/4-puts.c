22fff877281c0132c86ed16f55d1b8e39edf0265
root <Nicolaslegoat>
2023-06-16 02:20:00 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #!/bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*  main - Description pour main
+*  Return: Always 0 (succes)
+*/
 int main() {
     puts("Programming is like building a multilingual puzzle");
     return 0;
