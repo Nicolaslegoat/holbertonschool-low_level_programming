c4e8167ac99ab0f6db53d0469392b6e10690fada
root <Nicolaslegoat>
2023-06-16 02:23:24 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,7 +4,6 @@
 *  main - Description pour main
 *  Return: Always 0 (succes)
 */
-int main {
-    puts ("Programming is like building a multilingual puzzle");
+int main() {
+    puts("Programming is like building a multilingual puzzle");
     return 0;
-}
