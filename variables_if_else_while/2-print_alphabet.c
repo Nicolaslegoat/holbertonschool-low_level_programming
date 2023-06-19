4cb1e11395ccf98ef528c7e38944e57639685812
root <Nicolaslegoat>
2023-06-19 07:57:31 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*  main - Description pour main
+*  Return: Always 0 (succes)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	fot (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
