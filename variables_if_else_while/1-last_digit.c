0c90ad5d9faac1d708158b709c7a5432ec80bba7
root <Nicolaslegoat>
2023-06-19 05:59:40 -0700

    1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h> 
+/**
+* Main - All my programm
+* Return: Always(Success)
+*/
+int main(void)
+{
+	int n, last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if(last_digit > 5){
+	printf("%d and %d greater than 5/n", n, last_digit);
+	}
+	if (last_digit == o){
+	printf("%d and %d 0/n", n, last_digit)
+	}
+	if (last_digit <= 0){
+	printf ("%d and %d less than 6 and not 0/n", n, last_digit)
+	return (0);
+}
