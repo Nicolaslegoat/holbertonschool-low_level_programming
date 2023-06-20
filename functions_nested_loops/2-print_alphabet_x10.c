4d24cf9866721bfb1f78c4e3164fe0b0051902a9
root <Nicolaslegoat>
2023-06-20 06:18:12 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
-*  main - Description pour main
-*  Return: Always 0 (succes)
+ * print_alphabet_x10 - alphabet x10
+ * Return: Always 0 (succes)
 */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	_putchar(letter);
+	_putchar(letter);
+	_putchar(letter);
+	_putchar(letter);
+	_putchar(letter);
+	_putchar(letter);
+	_putchar(letter);
+	_putchar(letter);
+	_putchar(letter);
+	_putchar(letter);
+}
