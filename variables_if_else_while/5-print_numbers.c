766f63db9c604e537b2b17e41593eb6b0d4c7ec3
root <Nicolaslegoat>
2023-06-19 23:17:15 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Description pour main
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0' ; ch <= 9; ch++)
+	{
+		putchar(ch);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
