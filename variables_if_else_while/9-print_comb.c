b4c6c50e2dfa05ca1275d4bbe15fd05c1bf2c069
root <Nicolaslegoat>
2023-06-19 23:47:13 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Description pour main
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+		if (c != 57)
+	}
+	{
+		putchar(',')
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
