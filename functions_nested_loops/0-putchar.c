8ae19b60c4aa7d64d758a5a0dbe7c8ddbc7f6ccf
root <Nicolaslegoat>
2023-06-20 02:26:58 -0700

    functions_nested_loops/

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <main.h>
 
 /**
- *  main - Description pour main
- *  Return: Always 0 (succes)
+ * main - Description pour main
+ * Return: Always 0 (succes)
  */
 
 int main(void)
 {
 	int a;
-
-	for (a ; "_putchar" ; a++)
 	{
-		putchar(a);
+		_putchar("_");
+		_putchar("p");
+		_putchar("u");
+		_putchar("t");
+		_putchar("c");
+		_putchar("h");
+		_putchar("a");
+		_putchar("r");
+		_putchar("\n");
 
 	}
 	return (0);
