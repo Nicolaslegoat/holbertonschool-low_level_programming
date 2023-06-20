7fad1b079fed43290d764c7f3ba28c5d13e2abe9
root <Nicolaslegoat>
2023-06-20 12:19:54 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -3,23 +3,20 @@
 /**
  * print_alphabet_x10 - alphabet x10
  * Return: Always 0 (succes)
-*/
+ */
 
 void print_alphabet_x10(void)
 {
-	char letter;
+	int count = 0
+		char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	while (count++ <= 9)
 	{
-	_putchar(letter);
-	_putchar(letter);
-	_putchar(letter);
-	_putchar(letter);
-	_putchar(letter);
-	_putchar(letter);
-	_putchar(letter);
-	_putchar(letter);
-	_putchar(letter);
-	_putchar(letter);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n')
 	}
+
+
 }
+
