b3417ce46508e38f1025d558eb216da6a4737e68
root <Nicolaslegoat>
2023-06-19 23:27:03 -0700

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Description pour main
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 122; a >= 97; a++)
+	{
+		putchar(a);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
