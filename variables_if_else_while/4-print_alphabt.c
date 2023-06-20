c2571d5b0cfedaec025f07b54a673a42608785ec
root <Nicolaslegoat>
2023-06-19 23:08:50 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Description pour main
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	if (a == 113 ; a == 101)
+	{
+		putchar('\n');
+	}
+	return (0);
