1987629c7fe5f78f15969c5a0ec8a3bcad97f757
root <Nicolaslegoat>
2023-06-20 01:18:16 -0700

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *  main - Description pour main
+ *  Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a ; "_putchar" ; a++)
+	{
+		putchar(a);
+
+	}
+	return (0);
+}
