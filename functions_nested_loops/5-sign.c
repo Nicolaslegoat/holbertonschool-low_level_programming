b592bddcfa87459fb52a3367845f791f90f23862
root <Nicolaslegoat>
2023-06-20 15:19:26 -0700

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,18 +9,15 @@
 int print_sign(int n)
 {
         if (n > 0){
-                printf("+", n);
-        }
-        {       return (1);
+	_putchar('+');
+	return (1);
         }
         if (n == 0){
-                printf("0", n);
-        }
-        {       return (0);
+                _putchar('0');
+		return (0);
         }
         else (n < 0){
-                printf("-", n)
-        }
-        {
-                return (0);}
+                _putchar('-');
+                return (0);
+	}
 }
