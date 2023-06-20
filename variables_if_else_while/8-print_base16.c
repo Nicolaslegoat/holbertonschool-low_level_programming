c9be5a4c5503865efce75e385959be18dc6c1639
root <Nicolaslegoat>
2023-06-19 23:40:40 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Description pour main
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
