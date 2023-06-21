cf52497ea21c1085db5ca6376f5785823887b847
root <Nicolaslegoat>
2023-06-21 02:37:11 -0700

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
-*  main - Description pour main
-*  Return: Always 0 (succes)
-*/
+* print_last_digit - Description pour main
+* @n: Fonction
+* Return: Always 0 (succes)
+ */
+int print_last_digit(int n);
+{	int ld = n % 10
+
+	if (ld < 0)
+		ld *= -1;
+	_putchar(ld + '0');
+		return (ld);
+}
