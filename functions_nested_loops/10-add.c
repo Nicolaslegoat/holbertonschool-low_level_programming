2514df43a0fcdc10c84707f7d5483d0915f485b5
root <Nicolaslegoat>
2023-06-22 01:53:42 -0700

    10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-*  add - Description pour main
-*  Return: Always 0 (succes)
-*/
+ * add - Description pour main
+ * Return: Always 0 (succes)
+ */
 
 int add(int num1, int num2)
 {
