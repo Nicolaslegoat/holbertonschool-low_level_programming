eb8d2d9eb1158dcb44cc34411b60b657cfb21a03
root <Nicolaslegoat>
2023-06-22 14:10:44 -0700

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isdigit - prototype fonction
+* @c: variable
+* Return: 1 if character is digit, 0 othrerwise.
+*/
+
+
+int _isdigit(int c)
+{
+        if (c >= 48 && c <= 57)
+
+                return (1);
+
+        else
+                return (0);
+}
