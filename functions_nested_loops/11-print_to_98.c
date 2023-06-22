70b3168f1a08726f83928d36fd3393996fb3abb7
root <Nicolaslegoat>
2023-06-22 02:16:58 -0700

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+#include "main.h"
 /**
-*  main - Description pour main
-*  Return: Always 0 (succes)
-*/
+ * print_to_98 - Description pour main
+ *  Return: Always 0 (succes)
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
