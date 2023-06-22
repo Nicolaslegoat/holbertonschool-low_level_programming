1e451d92a3628d2d326f7d794ace1210cffb55e5
root <Nicolaslegoat>
2023-06-22 01:17:33 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,8 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
-*  main - Description pour main
-*  Return: Always 0 (succes)
-*/
+ * times_table - Fonction
+ *
+ */
+void times_table(void)
+	int num, multi, prod;
+
+	for (num = 0; num <= 9; num++)
+{
+	_putchar('0');
+	{
+		for (multi = 1; multi <= 9; multi++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * multi;
+
+			if (prod <= 9)
+				_puthar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+
+}
+
