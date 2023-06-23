fe086846590853eb0bb71ed31fc30ec0dd2ceded
root <Nicolaslegoat>
2023-06-23 04:33:08 -0700

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - Prints the numbers from 0-9.
+ */
+void more_numbers(void)
+{
+	int line = 0;
+	int num;
+
+	while (line++ <= 9)
+	{
+		for (num = 0; num <= 14; num++)
+			_putchar((num % 10) + '0');
+
+		_putchar('\n');
+	}
+}
+
