d3f82f710425603eff8104dd7cc9e9eefb2d88f0
root <Nicolaslegoat>
2023-06-23 04:23:19 -0700

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,14 @@
 /**
  * print_most_numbers - Prints the numbers from 0-9.
  */
-void print_most_umbers(void)
+void print_most_numbers(void)
 {
 	int num;
 
 	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num != 4)
-		{
 			_putchar((num % 10) + '0');
-		}
-		_putchar('\n');
 	}
+		_putchar('\n');
 }
