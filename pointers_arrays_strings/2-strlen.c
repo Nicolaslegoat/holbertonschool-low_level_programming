9bdf54dca3ef749ab9126812a8248409ac9e113d
root <Nicolaslegoat>
2023-06-27 02:10:41 -0700

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strlen - fonction
+ * @s: var
+ */
+int _strlen(char *s)
+{
+	size_t length = 0;
+	while (*s++){
+		length++;
+	}
+	return length;
+}
