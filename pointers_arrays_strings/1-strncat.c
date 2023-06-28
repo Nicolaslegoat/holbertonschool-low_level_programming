dcd1e89b41333e4225158f0fc294c9f21be20829
root <Nicolaslegoat>
2023-06-28 07:56:12 -0700

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,7 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i++])
+	while (dest[i++] != '0')
 		j++;
 	for (i = 0; src[i] && i < n; i++)
 		dest[j] = src[i];
