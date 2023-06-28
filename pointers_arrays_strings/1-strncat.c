4c74ec52ae333a5047d6af73d10f0b088cc3c99f
root <Nicolaslegoat>
2023-06-28 07:53:12 -0700

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncat - fonction
+ * @dest: var
+ * @src; var
+ * @n: var
+ */
+char *_strncat(char *dest, char *src, int n)
+{       
+	int i = 0;
+	int j = 0;
+
+	while (dest[i++])
+		j++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[j] = src[i];
+
+	return (dest);
