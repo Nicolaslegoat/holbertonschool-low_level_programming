c457ef5528817dafa77cbf57455a999a6c2903ae
root <Nicolaslegoat>
2023-06-29 08:48:30 -0700

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcmp - fct
+ * @s1: var
+ * @s2: var
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
