b26cf555c87039b62dfb1257194ea2da6888461b
root <Nicolaslegoat>
2023-06-30 06:40:23 -0700

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _strchr - fct
+ * @s: var
+ * @c: var
+ * Return: Return to 0
  */
 char *_strchr(char *s, char c)
 {
 	int i;
-	
+
 	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
