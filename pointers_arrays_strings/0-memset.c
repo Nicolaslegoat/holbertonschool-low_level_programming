f1f7461af84b623fc876c1ad6146e5237576a9fd
root <Nicolaslegoat>
2023-06-30 06:05:29 -0700

    0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fct
+ * @s: var
+ * @b: var
+ * @n/ var
+ * Return: memory here
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{	s[i] = b;
+	}
+	return (s);
+}
