c090225d366c9d04e559010bc62d990a9b729c63
root <Nicolaslegoat>
2023-07-07 06:56:31 -0700

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - fct
+ * @argc: var
+ * @argv: var
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+			return(0);
+}
