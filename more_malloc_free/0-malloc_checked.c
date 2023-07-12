ff5d2ac6a80e88ac654a71230ad23d3cb72aeef5
root <Nicolaslegoat>
2023-07-12 01:40:04 -0700

    more_malloc_free/

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * malloc_checked - fct
- * @b: var
- *
- */
+* malloc_checked - fct
+* @b: var
+* Return: nada
+*/
 void *malloc_checked(unsigned int b)
 {
 	void *p;
 
-	p = malloc(b);	
+	p = malloc(b);
 
 	if (p == NULL)
 
 		exit(98);
 
-	return(p);
+	return (p);
 }
