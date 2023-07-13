fe38a92e67872a1f295d161cf69a101fd72eac00
root <Nicolaslegoat>
2023-07-13 01:01:34 -0700

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - fct
+ * @nmemb: the number of elements
+ * @size: the bytes size of each array elements
+ * Return: If nmemb = 0, size = 0, or the function fails.
+ *
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+	char *s;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
+		return (NULL);
+
+	s = mem;
+
+	for (i = 0; i < (size * nmemb); i++)
+		s[i] = '\0';
+	
+	return (mem);
+}
