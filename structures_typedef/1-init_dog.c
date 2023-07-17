a31f6149e255de7f210502db40004c9bb61b1f86
root <Nicolaslegoat>
2023-07-17 14:54:19 -0700

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+include <stdlib.h>
+include "dog.h"
+/**
+ * init_dog - fct qui creer une var
+ * @d; pointeur general
+ * @name: nom du chien
+ * @age: age du chien
+ * @owner: proprio
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
