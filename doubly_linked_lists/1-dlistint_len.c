5a8b158eac71793386128e4f1cb583d2acdc6993
root <Nicolaslegoat>
2023-08-02 06:58:40 -0700

    betty pb

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * dlistint_len - fct to list elements
- * @h: ptr to list elements 
+ * @h: ptr to list elements
  * Return: the elements listed
  */
 size_t dlistint_len(const dlistint_t *h)
