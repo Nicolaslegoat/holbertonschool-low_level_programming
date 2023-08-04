204473411ed9b214b9c9878f91e9494e4b1b54ba
root <Nicolaslegoat>
2023-08-04 12:51:17 -0700

    modify file

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,23 +8,14 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size > 0)
-
+	for (i = 0; i < size; i++)
 	{
-
-		for (i = 1; i <= size; i++)
-		{
-			for (j = size - i; j > 0; j++)
-
-				_putchar(' ');
-
-			for (j = 0; j < i; j++)
-				_putchar('#');
-
-			if (i == j)
-				continue;
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
 			_putchar('\n');
-		}
 	}
 	_putchar('\n');
 }
