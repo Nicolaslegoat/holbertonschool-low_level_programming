8e859337aea4a601c771e2afa8a5b59c616d01cf
root <Nicolaslegoat>
2023-08-04 12:34:40 -0700

    add NULL

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * binary_to_uint - convert a binary number
  * @b: a pointer to a string
@@ -9,16 +10,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, multi = 1;
 	int length;
 
-	if (*b == '\0')
+	if (b == NULL)
 		return (0);
 
-	for (length = 0; b[length];)
-		length++;
-
-	for (length -= 1; length >= 0; length--)
+	for (length = 0; b[length]; length++)
 	{
 		if (b[length] != '0' && b[length] != '1')
 			return (0);
+	}
+
+	for (length -= 1; length >= 0; length--)
+	{
 
 		num += (b[length] - '0') * multi;
 		multi *= 2;
