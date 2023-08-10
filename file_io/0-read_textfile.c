73a6e0856bed74f6696ae12fe5fe047784643bd6
root <Nicolaslegoat>
2023-08-10 01:29:26 -0700

    betty pb

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * read_textefile - Read a text file and prints it to POSIX stdout
+ * read_textfile - Read a text file and prints it to POSIX stdout
  * @filename: a pointer to the name of file
  * @letters: the number of letters
  * Return: the number of bytes the function can read and print
@@ -23,10 +23,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	Write = write(STDOUT_FILENO, buffer, Read);
 
 	if (Open == -1 || Read == -1 || Write == -1 || Write != Read)
-		{
-			free(buffer);
-			return (0);
-		}
+	{
+		free(buffer);
+		return (0);
+	}
 
 	free(buffer);
 	close(Open);
